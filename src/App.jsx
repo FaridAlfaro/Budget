32806b9d61fdf6ca41b6c4ca857e31b7acbafc81
Farid Alfaro <farix6@gmail.com>
2023-04-22 16:08:28 -0300

    Nav Fin 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import '../css/main.css'
 import NavBar from './components/NavBar'
-import Carousel from './components/Carousel'
 function App() {
   return (
     <>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,40 +1,40 @@
 
-function Carousel() {
-  const images = [
-    {
-      src: '../../img/header/1.png',
-      alt: 'Static Image 1',
-    },
-    {
-      src: '../../img/header/2.png',
-      alt: 'Static Image 2',
-    },
-    {
-      src: '../../img/header/3.png',
-      alt: 'Static Image 3',
-    }
-  ];
-  return (
-    <section className="main-slider">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className={`item image`}
-          style={image.style}
-        >
-          <span className="loading">Loading...</span>
-          <figure>
-            <div className="slide-image slide-media">
-              <img src={image.src} alt={image.alt} />
-            </div>
-            <figcaption className="caption">{image.alt}</figcaption>
-          </figure>
-        </div>
-      ))}
-      <button className="prev">Previous</button>
-      <button className="next">Next</button>
-    </section>
-  );
-}
+// function Carousel() {
+//   const images = [
+//     {
+//       src: '../../img/header/1.png',
+//       alt: 'Static Image 1',
+//     },
+//     {
+//       src: '../../img/header/2.png',
+//       alt: 'Static Image 2',
+//     },
+//     {
+//       src: '../../img/header/3.png',
+//       alt: 'Static Image 3',
+//     }
+//   ];
+//   return (
+//     <section className="main-slider">
+//       {images.map((image, index) => (
+//         <div
+//           key={index}
+//           className={`item image`}
+//           style={image.style}
+//         >
+//           <span className="loading">Loading...</span>
+//           <figure>
+//             <div className="slide-image slide-media">
+//               <img src={image.src} alt={image.alt} />
+//             </div>
+//             <figcaption className="caption">{image.alt}</figcaption>
+//           </figure>
+//         </div>
+//       ))}
+//       <button className="prev">Previous</button>
+//       <button className="next">Next</button>
+//     </section>
+//   );
+// }
 
-export default Carousel;
+// export default Carousel;
