6ad944b702667a2d61fad79ab1db1d719423d3fa
FaridAlfaro <farix6@gmail.com>
2023-04-22 22:02:29 -0300

    Carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import '../css/main.css'
 import NavBar from './components/NavBar'
+import '../css/pruebas.css'
+import Carousel from './components/Carousel'
+import '../img/header/1.png'
 function App() {
+  const images = [
+    "../img/header/1.png",
+    "../img/header/2.png",
+    "../img/header/3.png"
+  ];
   return (
     <>
       <NavBar />
+      <Carousel images={images} />
     </>
   )
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -38,3 +38,46 @@
 // }
 
 // export default Carousel;
+
+
+import React, { useState } from "react";
+function Carousel ({ images }) {
+
+        const [currentImageIndex, setCurrentImageIndex] = useState(0);
+      
+        const handleNextImage = () => {
+          setCurrentImageIndex((currentImageIndex + 1) % images.length);
+        };
+      
+        const handlePrevImage = () => {
+          setCurrentImageIndex(
+            currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1
+          );
+        };
+      
+        return (
+          <div className="carousel-container">
+            <img
+              className="carousel-image current-image"
+              src={images[currentImageIndex]}
+              alt={`Image ${currentImageIndex + 1}`}
+            />
+            <img
+              className="carousel-image next-image"
+              src={
+                images[(currentImageIndex + 1) % images.length]
+              }
+              alt={`Image ${currentImageIndex + 2}`}
+            />
+            <button className="carousel-button left" onClick={handlePrevImage}>
+              {"<"}
+            </button>
+            <button className="carousel-button right" onClick={handleNextImage}>
+              {">"}
+            </button>
+          </div>
+        );
+      };
+
+
+export default Carousel;
