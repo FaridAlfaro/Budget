0e77adebc6161fd4dee5ba26dd50b788531c9fd1
FaridAlfaro <farix6@gmail.com>
2023-04-23 18:25:24 -0300

    carousel fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import '../css/main.css'
-import NavBar from './components/NavBar'
-import Carousel from './components/Carousel'
-import '../img/header/1.png'
+import React, { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "../css/main.css";
+import NavBar from "./components/NavBar";
+import Carousel from "./components/Carousel";
+import img1 from "../img/header/1.png";
+import img2 from "../img/header/2.png";
+import img3 from "../img/header/3.png";
+
 function App() {
-  const images = [
-    "../img/header/1.png",
-    "../img/header/2.png",
-    "../img/header/3.png"
-  ];
+  const images = [img1, img2, img3];
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
     <>
       <NavBar />
       <Carousel images={images} />
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,45 +1,3 @@
-
-// function Carousel() {
-//   const images = [
-//     {
-//       src: 'https://i.ibb.co/k1BfGw3/1.png',
-//       alt: 'Static Image 1',
-//     },
-//     {
-//       src: 'https://i.ibb.co/f2fCh9T/2.png',
-//       alt: 'Static Image 2',
-//     },
-//     {
-//       src: 'https://i.ibb.co/CBHDSXp/3.png',
-//       alt: 'Static Image 3',
-//     }
-//   ];
-//   return (
-//     <section className="main-slider">
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           className={`item image`}
-//           style={image.style}
-//         >
-//           <span className="loading">Loading...</span>
-//           <figure>
-//             <div className="slide-image slide-media">
-//               <img src={image.src} alt={image.alt} />
-//             </div>
-//             <figcaption className="caption">{image.alt}</figcaption>
-//           </figure>
-//         </div>
-//       ))}
-//       <button className="prev">Previous</button>
-//       <button className="next">Next</button>
-//     </section>
-//   );
-// }
-
-// export default Carousel;
-
-
 import React, { useState } from "react";
 function Carousel ({ images }) {
 
@@ -56,7 +14,10 @@ function Carousel ({ images }) {
         };
       
         return (
-          <div className="carousel-container">
+          <div className="carousel-container" data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-delay="300"
+          data-aos-offset="0">
             <img
               className="carousel-image current-image"
               src={images[currentImageIndex]}
