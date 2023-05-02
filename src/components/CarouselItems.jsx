430190488c5ec6fb26df000d3c367d7d074a1c3e
FaridAlfaro <farix6@gmail.com>
2023-05-02 18:08:27 -0300

    Segunda ruta correcion

--- src/components/CarouselItems.jsx
+++ src/components/CarouselItems.jsx
@@ -3,6 +3,7 @@ import { Button } from 'primereact/button';
 import { Carousel } from 'primereact/carousel';
 import { Tag } from 'primereact/tag';
 import { ProductService } from './Items';
+export const IMAGE_PATH = "../../img/items/";
 function CircularDemo() {
     const [products, setProducts] = useState([]);
     const responsiveOptions = [
@@ -47,7 +48,7 @@ function CircularDemo() {
         return (
             <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
                 <div className="mb-3">
-                    <img src={`../../img/items/${product.image}`} alt={product.name} className="w-6 shadow-2" height={'200px'}/>
+                    <img src={`${IMAGE_PATH}${product.image}`} alt={product.name} className="w-6 shadow-2" height={'200px'}/>
                 </div>
                 <div>
                     <h4 className="mb-1">{product.name}</h4>
